09f9067eecba7664187e29b0dc6e396a00dc3580
dommich <dominik.mich@autonomik.pl>
2016-05-21 14:49:36 +0200

    Add db connection.

---
+++ src/stazkainos/Models/Fund.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace stazkainos.Models
+{
+    public class Fund
+    {
+        public DateTime Data { get; set; }
+        public double Wartosc { get; set; }
+    }
+    public class stazkainos_db : DbContext
+    {
+        public DbSet<Fund> Funds { get; set; }
+    }
+}
